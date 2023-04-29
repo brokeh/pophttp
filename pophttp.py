4f7553e0192848e19735c8f45a341caaa819ae75
brokeh <git@brocky.net>
2023-04-29 18:23:58 +1000

    Don't take exclusive use of the Lifx port on the server

--- pophttp.py
+++ pophttp.py
@@ -119,6 +119,7 @@ class MessageHandler(object):
 
 def server_loop(address, handler):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((address, 56700))
     print('Server started on on %s' % address)
     while True:
